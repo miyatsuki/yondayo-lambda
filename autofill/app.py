e1345a5ded46e85fcc994559a399952464a41b91
miyatsuki <miyatsuki.shiku@gmail.com>
2021-10-27 07:35:19 +0900

    amazonのurlの場合、isbnコードから書影を取得する

--- autofill/app.py
+++ autofill/app.py
@@ -16,13 +16,49 @@ def parse_head(url: str):
     return {"url": url, "title": title, "imageURL": image_url}
 
 
+def fetch_openBD_info(isbn10: str):
+    r = requests.get(f"https://api.openbd.jp/v1/get?isbn={isbn10}")
+    data = r.json()
+
+    title = data[0]["summary"]["title"]
+    imageURL = data[0]["summary"]["cover"]
+    total = data[0]["onix"]["DescriptiveDetail"]["Extent"][0]["ExtentValue"]
+
+    return {"title": title, "imageURL": imageURL, "total": total}
+
+
+def is_isbn(code: str):
+    # 全部数字ならISBNとみなす
+    if code.isdigit():
+        return True
+    # 最終桁以外数字で最後がXの場合はISBNとみなす
+    elif code[:-1].isdigit() and code[-1] == "X":
+        return True
+    else:
+        return False
+
+
 def handler(event, context):
     print(event)
     payload = event["body"]
     print(payload)
     data = json.loads(payload)
+    url: str = data["url"]
 
-    info = parse_head(data["url"])
+    info = {}
+    if url.startswith("https://www.amazon.co.jp/"):
+        path = url.split("/")
+        isbn = ""
+        for i in range(1, len(path)):
+            if path[i - 1] == "dp" and is_isbn(path[i]):
+                isbn = path[i]
+                break
+        if isbn:
+            info = fetch_openBD_info(isbn)
+            info["url"] = url
+    if not isbn:
+        info = parse_head(url)
+
     return {"statusCode": 200, "body": json.dumps(info)}
 
 
