b0d1c9e8d46cf07b527aeac9e3ab52016781731b
miyatsuki <miyatsuki.shiku@gmail.com>
2021-11-03 13:00:03 +0900

    CORSの設定追加

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import datetime
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import autofill
 import image
 import proceed
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://yondayo-api.herokuapp.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
