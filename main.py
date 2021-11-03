56abfbfc94ad1a810adf1c1482d89fcd3a0a8c52
miyatsuki <miyatsuki.shiku@gmail.com>
2021-11-03 12:00:05 +0900

    imageをもう一度外す

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import datetime
 from fastapi import FastAPI
 import autofill
-import image
 
+# import image
 # import proceed
 
 app = FastAPI()
@@ -28,8 +28,9 @@ async def get_image(user_name: str, start_date: str, end_date: str):
     # TODO: timezoneの明示
     start_date = datetime.datetime.strptime(start_date, "%Y%m%d")
     end_date = datetime.datetime.strptime(end_date, "%Y%m%d")
-    url = image.handle(user_name, start_date, end_date)
-    return {"url": url}
+    # url = image.handle(user_name, start_date, end_date)
+    # return {"url": url}
+    return {"message": "Hello World!!"}
 
 
 @app.get("/autofill")
